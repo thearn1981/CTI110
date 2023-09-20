0342aeb1a16caa737c1ee389c8fc9dc9ef53b4e7
thearn1981 <145587836+thearn1981@users.noreply.github.com>
2023-09-20 11:50:06 -0400

    Add files via upload
    First Submission!

---
+++ P1LAB1_HearnTeresa.py
@@ -0,0 +1,9 @@
+firstname=input("Enter your first name:  ")
+
+print("Good Morning", firstname,"!")
+
+num1=float(input("Enter The First Number:  "))
+
+num2=float(input("Enter The Second Number:  "))
+num3=num1+num2
+print("The Sum is ", num3)
