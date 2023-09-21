a89951aab46b1262b755ff55f9a8d6a062c1055e
thearn1981 <145587836+thearn1981@users.noreply.github.com>
2023-09-21 08:11:27 -0400

    Add files via upload
    P1HW1 assignment

---
+++ P1HW1.py
@@ -0,0 +1,13 @@
+user_num = int(input('Enter integer:\n'))
+
+print("You entered:",user_num)
+squaredNum=user_num**2
+print(user_num, "squared is", squaredNum)
+cubedNum=user_num**3
+print("And", user_num, "cubed is", cubedNum,"!!")
+user_num2=int(input("Enter another integer:\n"))
+sum=user_num+user_num2
+print(user_num,"+",user_num2,"is",sum)
+factor=user_num*user_num2
+print(user_num,"*",user_num2,"is",factor)
+
