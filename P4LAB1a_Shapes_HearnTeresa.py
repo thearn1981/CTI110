c1dd80e93b6ff05e26280ca3a656b20c04497368
thearn1981 <145587836+thearn1981@users.noreply.github.com>
2023-11-29 09:25:28 -0500

    P4LAB1A&B-Shapes and Initials

---
+++ P4LAB1a_Shapes_HearnTeresa.py
@@ -0,0 +1,19 @@
+import turtle
+sam=turtle.Turtle()
+sam.shape("turtle")
+
+for num in range(4):
+    sam.forward(50)
+    sam.left(90)
+  
+
+sam.penup()
+sam.forward(100)
+sam.pendown()
+
+for num in range (3):
+
+    sam.forward(50)
+    sam.left(120)
+
+    
